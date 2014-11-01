e1d7c671fa0acf2fb75b4ab714899f3d09973481
diver-sity <li.zhao7@yahoo.com.au>
2014-11-01 17:54:26 +1100

    refactored code and tests; tests all passed

--- src/main/java/org/owasp/webgoat/controller/AccessController.java
+++ src/main/java/org/owasp/webgoat/controller/AccessController.java
@@ -6,6 +6,8 @@
 package org.owasp.webgoat.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,24 +15,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- *
- * @author rlawson
+ * @author rlawson, Lee Simon
  */
 @Controller
-public class Login {
+public class AccessController {
+
+    public static final String LOG_OUT_MESSAGE = "You've been logged out successfully.";
+    private final Logger logger = LoggerFactory.getLogger(AccessController.class);
 
     @RequestMapping(value = "login.mvc", method = RequestMethod.GET)
     public ModelAndView login(
             @RequestParam(value = "error", required = false) String error,
             @RequestParam(value = "logout", required = false) String logout) {
+        return createModelAndView(error, logout, "login");
+    }
 
-        ModelAndView modelAndView = new ModelAndView("login");
+    @RequestMapping(value = "logout.mvc", method = RequestMethod.GET)
+    public ModelAndView logout(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+        logger.info("Logging user out");
+        return createModelAndView(error, logout, "logout");
+    }
+
+    private ModelAndView createModelAndView(String error, String logout, String viewName) {
+        ModelAndView modelAndView = new ModelAndView(viewName);
         if (StringUtils.isNotEmpty(error)) {
             modelAndView.addObject("error", "Invalid username and password!");
         }
         if (StringUtils.isNotEmpty(logout)) {
-            modelAndView.addObject("msg", "You've been logged out successfully.");
+            modelAndView.addObject("msg", LOG_OUT_MESSAGE);
         }
         return modelAndView;
     }
+    
 }
--- src/test/java/org/owasp/webgoat/controller/AccessControllerTest.java
+++ src/test/java/org/owasp/webgoat/controller/AccessControllerTest.java
@@ -18,20 +18,20 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@ContextConfiguration(locations={
-        "/mvc-dispatcher-servlet.xml"
-})
+@ContextConfiguration(locations={"/mvc-dispatcher-servlet.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration(value = "web")
-public class LoginTest {
+public class AccessControllerTest {
 
     @Autowired
     private WebApplicationContext wac;
     private MockMvc mvc;
+    private final String LOGIN_URL = "/login.mvc";
+    private final String LOGOUT_URL = "/logout.mvc";
 
     @Before
     public void setUp(){
@@ -40,13 +40,39 @@ public class LoginTest {
 
     @Test
     public void testLogin() throws Exception {
-        MockHttpServletRequestBuilder requestBuilder =
-                MockMvcRequestBuilders.get("/login.mvc");
+        MockHttpServletRequestBuilder requestBuilder = createRequest(LOGIN_URL);
         requestBuilder.param("error", "an error");
         requestBuilder.param("logout", "logout model attribute");
         ResultActions result = mvc.perform(requestBuilder);
-        checkView(result);
+        checkView(result, "login");
         checkModel(result);
+    }
+    
+    @Test
+    public void testLoginWithoutError() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = createRequest(LOGIN_URL);
+        ResultActions result = mvc.perform(requestBuilder);
+        checkView(result, "login");
+        Map<String, Object> model = result.andReturn().getModelAndView().getModel();
+        assertFalse(model.containsKey("error"));
+    }
+    
+    @Test
+    public void testLogout() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = 
+                createRequest(LOGOUT_URL);
+        requestBuilder.param("logout", "logout");
+        ResultActions result = mvc.perform(requestBuilder);
+        checkView(result, "logout");
+        Map<String, Object> model = result.andReturn().getModelAndView().getModel();
+        assertTrue(model.containsKey("msg"));
+        assertEquals(AccessController.LOG_OUT_MESSAGE,model.get("msg"));
+    }
+
+    private MockHttpServletRequestBuilder createRequest(String url) {
+        MockHttpServletRequestBuilder requestBuilder =
+                MockMvcRequestBuilders.get(url);
+        return requestBuilder;
     }
 
     private void checkModel(ResultActions result) {
@@ -56,8 +82,7 @@ public class LoginTest {
         assertTrue(model.containsKey("msg"));
     }
 
-    private void checkView(ResultActions result) throws Exception {
-        result.andExpect(status().isOk())
-        .andExpect(MockMvcResultMatchers.view().name("login"));
+    private void checkView(ResultActions result, String viewName) throws Exception {
+        result.andExpect(status().isOk()).andExpect(view().name(viewName));
     }
 }
